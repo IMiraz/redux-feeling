7eab24d2da5a5be8d1531fbf4a23aaff8d045aeb
wasifmiraz <wasifmiraz@mail.com>
2018-08-07 22:06:12 +0600

    bind up

--- src/component/postForm.js
+++ src/component/postForm.js
@@ -3,25 +3,34 @@ import React, { Component } from 'react'
 class PostForm extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            title:'',
+            body:'',
+        };
 
+this.onChange = this.onChange.bind(this);
     }
+
+onChange(e) {
+ this.setState({[e.target.name]:e.target.value});
+}
 
   render() {
 
 
     return (
       <div>
-<h1>Postform</h1>
+<h1>Add Post</h1>
 <form>
 <div>
 <label>title:</label>
 <br/>
-<input type="text" name="title"/>
+<input type="text" name="title"  value = {this.state.title} onChange={this.onChange}/>
 </div>
 <div>
 <label>Body:</label>
 <br/>
-<textarea name="body"/>
+<textarea name="body" value= {this.state.body} onChange={this.onChange}/>
 </div>
 <br/>
 <button type="submit">post</button>
