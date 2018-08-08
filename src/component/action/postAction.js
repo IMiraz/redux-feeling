05272ccbe06860685ec60adc94dca40c3b198b41
wasifmiraz <wasifmiraz@mail.com>
2018-08-08 15:50:39 +0600

    postaction up

---
+++ src/component/action/postAction.js
@@ -0,0 +1,16 @@
+import {FETCH_POSTS, NEW_POST} from './type'
+
+export function fetchPosts()
+{
+    return  function(dispatch) {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(posts =>dispatch({
+            type:FETCH_POSTS,
+            payLoad:posts
+
+        }))
+
+    }
+
+}
--- src/component/post.js
+++ src/component/post.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
-    constructor(props) {
-        super(props)
-        this.state ={
-            post:[],
-        }
-    }
-componentWillMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => this.setState({post:data}))
-}
+
+
+
   render() {
 
  const postitem = this.state.post.map(data => (
