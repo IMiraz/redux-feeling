e0d467981157488b3b6a2a7256349a0282a3131a
wasifmiraz <wasifmiraz@mail.com>
2018-08-08 20:51:43 +0600

    dispatch up

--- src/component/action/dispatch.js
+++ src/component/action/dispatch.js
@@ -1,5 +1,7 @@
 import {FETCH_POSTS, NEW_POST} from './type'
 
+//fetchData
+
 export const  fetchPosts =() =>dispatch =>
 {
         fetch('https://jsonplaceholder.typicode.com/posts')
@@ -7,6 +9,26 @@ export const  fetchPosts =() =>dispatch =>
         .then(posts =>dispatch({
             type:FETCH_POSTS,
             payLoad:posts
+
+        }))
+}
+
+//addedPost
+export const  createPost=(postData) =>dispatch =>
+{
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method:'POST',
+        headers:{
+            'content-type': 'application/Json'
+        },
+        body:JSON.stringify(postData)
+         })
+
+         .then(res => res.json())
+
+         .then(post =>dispatch({
+            type:NEW_POST,
+            payLoad:post
 
         }))
 }
